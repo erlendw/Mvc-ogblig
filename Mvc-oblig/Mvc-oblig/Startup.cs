944a5beda32e8e2e55f5ef58f38df43e39dd662b
erlendw <erlendwestbye@gmail.com>
2015-09-23 09:56:29 +0200

    Første commit

---
+++ Mvc-oblig/Mvc-oblig/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mvc_oblig.Startup))]
+namespace Mvc_oblig
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
