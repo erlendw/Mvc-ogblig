9ba672070d234ec62b243e7f976c9d37283a1dd2
erlendw <erlendwestbye@gmail.com>
2015-09-25 13:56:01 +0200

    Gjort masse greier, kan nå legge inn kunder i databasen. PS: Mads sitt verk

--- Mvc-oblig/Mvc-oblig/App_Start/RouteConfig.cs
+++ Mvc-oblig/Mvc-oblig/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Mvc_oblig
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Customer", action = "GetAllCustomeres", id = UrlParameter.Optional }
+                defaults: new { controller = "Customer", action = "GetAllCustomers", id = UrlParameter.Optional }
             );
         }
     }
--- Mvc-oblig/Mvc-oblig/Controllers/CustomerController.cs
+++ Mvc-oblig/Mvc-oblig/Controllers/CustomerController.cs
@@ -9,13 +9,58 @@ namespace Mvc_oblig.Controllers
     public class CustomerController : Controller
     {
         // GET: Customer
-        public ActionResult ListOfAllCustomers()
+        public ActionResult GetAllCustomers()
         {
             var db = new Models.CustomerContext();
-            List<Models.Customer> ListOfAllCustomers = db.Customer.ToList();
-            ViewData.Model = ListOfAllCustomers;
+            List<Models.Customer> GetAllCustomers = db.Customer.ToList();
+            ViewData.Model = GetAllCustomers;
+            //ViewBag.Message = "Click here " + GetAllCustomers[0].FirstName;
             ViewData["Message"] = "Do some stuff..and some shit: ";
             return View();
+        }
+        public ActionResult CreateCustomer()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateCustomer(FormCollection inList)
+        {
+            try
+            {
+                using (var db = new Models.CustomerContext())
+                {
+                    var newCustomer = new Models.Customer();
+                    newCustomer.FirstName = inList["FirstName"];
+                    newCustomer.LastName = inList["LastName"];
+                    newCustomer.Address = inList["Address"];
+                    // kan ikke bruke dette array i LINQ nedenfor
+                    string inZip = inList["ZipCode"];
+
+                    var foundPostalArea = db.PostalArea
+                   .FirstOrDefault(p => p.ZipCode == inZip);
+                    if (foundPostalArea == null) // fant ikke poststed, må legge inn et nytt
+                    {
+                        var newPostalArea = new Models.PostalArea();
+                        newPostalArea.ZipCode = inList["ZipCode"];
+                        newPostalArea.PostalArea_ = inList["PostalArea"];
+                        db.PostalArea.Add(newPostalArea);
+                        // det nye poststedet legges i den nye brukeren
+                        newCustomer.PostalArea = newPostalArea;
+                    }
+                    else
+                    { // fant poststedet, legger det inn i den nye brukeren
+                        newCustomer.PostalArea = foundPostalArea;
+                    }
+                    db.Customer.Add(newCustomer);
+                    db.SaveChanges();
+                    return RedirectToAction("GetAllCustomers");
+                }
+            }
+            catch (Exception e)
+            {
+                return View();
+            }
         }
     }
 }
