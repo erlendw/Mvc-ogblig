3e3da87b25f8f55c0110d822f5d6d8566dc32549
Bli5s <t.skill93@gmail.com>
2015-10-15 15:47:50 +0200

    Update Customermodels

--- Mvc-oblig/Mvc-oblig/Models/CustomerModels.cs
+++ Mvc-oblig/Mvc-oblig/Models/CustomerModels.cs
@@ -48,7 +48,7 @@ namespace Mvc_oblig.Models
 
         public CustomerContext() : base("name=customer")
         {
-            Database.CreateIfNotExists();
+            //Database.CreateIfNotExists();
 
         }
 
