b9b7d9839068cc0e32af28014b2592f0e4cec2cb
MadsKarlstad <mads.karlstad@me.com>
2015-10-16 10:45:49 +0200

    Slettet alt, lagt inn mitt. skummelt

---
+++ Sikkerhet/Controllers/SikkerhetController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sikkerhet.Models;
+
+namespace Sikkerhet.Controllers
+{
+    public class SikkerhetController : Controller
+    {
+        // GET: Sikkerhet
+        public ActionResult Index()
+        {
+            // vis innlogging
+            if (Session["LoggetInn"] == null)
+            {
+                Session["LoggetInn"] = false;
+                ViewBag.Innlogget = false;
+            }
+            else
+            {
+                ViewBag.Innlogget = (bool)Session["LoggetInn"];
+            }
+            return View();
+        }
+        public ActionResult Login()
+        {
+            // vis innlogging
+            if (Session["LoggetInn"] == null)
+            {
+                Session["LoggetInn"] = false;
+                ViewBag.Innlogget = false;
+            }
+            else
+            {
+                ViewBag.Innlogget = (bool)Session["LoggetInn"];
+            }
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(bruker innLogget)
+        {
+            // sjekk om innlogging OK
+            if (bruker_i_db(innLogget))
+            {
+                // ja brukernavn og passord er OK!
+                Session["LoggetInn"] = true;
+                ViewBag.Innlogget = true;
+                return View();
+            }
+            else
+            {
+                // ja brukernavn og passord er OK!
+                Session["LoggetInn"] = false;
+                ViewBag.Innlogget = false;
+                return View();
+            }
+        }
+        [HttpPost]
+        public ActionResult Index(bruker innLogget)
+        {
+            // sjekk om innlogging OK
+            if (bruker_i_db(innLogget))
+            {
+                // ja brukernavn og passord er OK!
+                Session["LoggetInn"] = true;
+                ViewBag.Innlogget = true;
+                return View();
+            }
+            else
+            {
+                // ja brukernavn og passord er OK!
+                Session["LoggetInn"] = false;
+                ViewBag.Innlogget = false;
+                return View();
+            }
+        }
+        public ActionResult Registrer()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Registrer(bruker innBruker)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            using (var db = new BrukerContext())
+            {
+                try
+                {
+                    var nyBruker = new dbBruker();
+                    byte[] passordDB = lagHash(innBruker.Passord);
+                    nyBruker.Navn = innBruker.Navn;
+                    nyBruker.Passord = passordDB;
+                    db.Brukere.Add(nyBruker);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                catch
+                {
+                    return View();
+                }
+            }
+        }
+        private static byte[] lagHash(string innPassord)
+        {
+            byte[] innData, utData;
+            var algoritme = System.Security.Cryptography.SHA256.Create();
+            innData = System.Text.Encoding.ASCII.GetBytes(innPassord);
+            utData = algoritme.ComputeHash(innData);
+            return utData;
+        }
+
+        private static bool bruker_i_db(bruker innBruker)
+        {
+            using (var db = new BrukerContext())
+            {
+                byte[] passordDB = lagHash(innBruker.Passord);
+                dbBruker funnetBruker = db.Brukere.FirstOrDefault(
+                    b => b.Passord == passordDB && b.Navn == innBruker.Navn);
+                if (funnetBruker == null)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+        public ActionResult InnloggetSide()
+        {
+            if (Session["LoggetInn"] != null)
+            {
+                bool loggetInn = (bool)Session["LoggetInn"];
+                if (loggetInn)
+                {
+                    return View();
+                }
+            }
+            return RedirectToAction("Index");
+        }
+        public ActionResult LoggUt()
+        {
+            Session["LoggetInn"] = false;
+            return RedirectToAction("index");
+        }
+    }
+}
---
+++ Sikkerhet/Models/bruker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Sikkerhet.Models
+{
+    public class bruker
+    {
+        [Required(ErrorMessage = "Navn må oppgis")]
+        public string Navn { get; set; }
+        [Required(ErrorMessage = "Passordmå oppgis")]
+        public string Passord { get; set; }
+    }
+
+    public class dbBruker
+    {
+        [Key]
+        public string Navn { get; set; }
+        public byte[] Passord { get; set; }
+    }
+
+    public class BrukerContext : DbContext
+    {
+        public BrukerContext()
+            : base("Bruker")
+        {
+            Database.CreateIfNotExists();
+        }
+
+        public DbSet<dbBruker> Brukere { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
