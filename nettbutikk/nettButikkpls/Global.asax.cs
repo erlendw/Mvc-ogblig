03e973db0535f806279e1528bc5cd296bd554af3
erlendw <erlendwestbye@gmail.com>
2015-10-18 16:08:47 +0200

    bootstrap kjører igjen

--- nettbutikk/nettButikkpls/Global.asax.cs
+++ nettbutikk/nettButikkpls/Global.asax.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace nettButikkpls
@@ -13,6 +14,9 @@ namespace nettButikkpls
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+
+        //http://stackoverflow.com/questions/21668280/how-do-i-add-bundleconfig-cs-to-my-project fix the bundle
     }
 }
