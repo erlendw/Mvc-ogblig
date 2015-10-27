641a664724ffaa5d46693f181797e23887f6f17a
danielthoresen <danielalex_97@hotmail.com>
2015-10-27 11:36:46 +0100

    legger til noen filer

---
+++ nettbutikk/nettButikkpls/DbOrder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using nettButikkpls.Models;
+using System.Web.Mvc;
+
+namespace nettButikkpls
+{
+    public class DbOrder
+    {
+        public void addToCart(int productid)
+        {
+            HttpContext context = HttpContext.Current;
+
+            if (context.Session["Cart"] == null)
+            {
+                Cart cart = new Cart();
+                cart.productids.Add(productid);
+                context.Session["Cart"] = cart;
+            }
+            else
+            {
+                Cart cart = (Cart)context.Session["Cart"];
+                cart.productids.Add(productid);
+            }
+        }         
+    }
+}
