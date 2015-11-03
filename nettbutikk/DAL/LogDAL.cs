7d58140f9616a9abf9238d155f67fe7ed773dcf4
MadsKarlstad <mads.karlstad@me.com>
2015-11-03 14:07:49 +0100

    Begynt på Log, Jokke fikk ikke til..typisk Jokke

---
+++ nettbutikk/DAL/LogDAL.cs
@@ -0,0 +1,20 @@
+﻿using nettButikkpls.DAL;
+using nettButikkpls.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace DAL
+{
+    class LogDAL
+    {
+        HttpContext context = HttpContext.Current;
+        NettbutikkContext bmx = new NettbutikkContext();
+    }
+}
+        
+    
+
