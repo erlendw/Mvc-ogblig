ebdefb5e7f11826dc93a12602b91b6e9cc788a61
Trym Skilleås <t.skill93@gmail.com>
2015-11-03 23:22:27 +0100

    Lagt til filer for testing/dependency injection

---
+++ nettbutikk/DAL/CustomerRepo.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using nettButikkpls.Models;
+using System.Security.Cryptography;
+using System.Diagnostics;
+using System.Web.Mvc;
+
+namespace nettButikkpls.DAL
+{
+    public class CustomerRepo
+    {
+        HttpContext context = HttpContext.Current;
+        NettbutikkContext bmx = new NettbutikkContext();
+        public List<Customer> allCustomers()
+        {
+            using (var db = new NettbutikkContext())
+            {
+                List<Customer> allCustomers = db.Customers.Select(c => new Customer
+                {
+                    customerId = c.CustomerId,
+                    firstname = c.Firstname,
+                    lastname = c.Lastname,
+                    address = c.Address,
+                    zipcode = c.Zipcode,
+                    isadmin = c.IsAdmin,
+                    postalarea = c.Postalareas.Postalarea,
+                    email = c.Mail,
+                    password = c.Password,
+                    salt = c.Salt
+                }).ToList();
+                return allCustomers;
+            }
+        }
+        public bool saveCustomer (Customer inCustomer)
+        {
+            using (var db = new NettbutikkContext())
+            {
+                try
+                {
+                    String salt = GenerateSalt(32);
+
+                    var newCustomerRow = new Customers();
+                    newCustomerRow.Mail = inCustomer.email;
+                    newCustomerRow.Password = HashPassword(inCustomer.password, salt);
+                    newCustomerRow.Firstname = inCustomer.firstname;
+                    newCustomerRow.Lastname = inCustomer.lastname;
+                    newCustomerRow.Address = inCustomer.address;
+                    newCustomerRow.IsAdmin = inCustomer.isadmin;
+                    newCustomerRow.Zipcode = inCustomer.zipcode;
+                    newCustomerRow.Salt = salt;
+
+                    var checkZipcode = db.PostalAreas.Find(inCustomer.zipcode);
+                    if(checkZipcode==null)
+                    {
+                        var postalareaRow = new PostalAreas();
+                        postalareaRow.Zipcode = inCustomer.zipcode;
+                        postalareaRow.Postalarea = inCustomer.postalarea;
+                        newCustomerRow.Postalareas = postalareaRow;
+                    }
+                    db.Customers.Add(newCustomerRow);
+                    db.SaveChanges();
+                    
+                    return true;
+                }catch(Exception feil)
+                {
+                    return false;
+                }                   
+            }  
+        }
+
+        public bool EditCustomer(FormCollection inList)
+        {
+            try
+                {
+                    // HttpContext context = HttpContext.Current;
+                    Customer c = (Customer)context.Session["CurrentUser"];
+                    Customers customer = FindCustomersByEmail(c.email);
+                    
+                    if (!(String.IsNullOrEmpty(inList["Mail"])))
+                    {
+                        customer.Mail = inList["Mail"];
+                    }
+                    if (!(String.IsNullOrEmpty(inList["Firstname"])))
+                    {
+                        customer.Firstname = inList["Firstname"];
+                }
+                    if (!(String.IsNullOrEmpty(inList["Lastname"])))
+                    {
+                        customer.Lastname = inList["Lastname"];
+                }
+                    if (!(String.IsNullOrEmpty(inList["Address"])))
+                    {
+                        customer.Address = inList["Address"];
+                }
+                    bmx.SaveChanges();
+                    c = FindCustomerByEmail(customer.Mail);
+                    context.Session["CurrentUser"] = c;
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    return false;
+                }
+        }
+        public bool Login()
+        {
+            //HttpContext context = HttpContext.Current;
+            if (context.Session["loggedin"] == null)
+            {
+                context.Session["loggedin"] = false;
+            }
+            else
+            {
+               return (bool)context.Session["loggedin"];
+            }
+            return false;
+        }
+        public bool ValidateUser(FormCollection inList)
+        {
+            Customer customer = FindCustomerByEmail(inList["Email"]);
+
+            if (customer != null)
+            {
+                String OldHash = customer.password;
+                String ReHash = HashPassword(inList["Password"], customer.salt);
+                HttpContext context = HttpContext.Current;
+                if (OldHash == ReHash)
+                {
+                    context.Session["loggedin"] = true;
+                    context.Session["CurrentUser"] = customer;
+                    Debug.WriteLine("Du er nå logget inn");
+                    return true;
+                    // return RedirectToAction("List");
+                }
+                else
+                {
+                    context.Session["loggedin"] = false;
+                    context.Session["CurrentUser"] = null;
+                    Debug.WriteLine("Kunne ikke logge inn");
+                    return false;
+                    //return RedirectToAction("Reg");
+                }
+            }
+            return false;
+            //return RedirectToAction("Login");
+        }
+        public String GenerateSalt(int size)
+        {
+            var RandomNumberGenerator = new System.Security.Cryptography.RNGCryptoServiceProvider();
+            var buffer = new byte[size];
+            RandomNumberGenerator.GetBytes(buffer);
+            return Convert.ToBase64String(buffer);
+        }
+        public String HashPassword(String Password, string Salt)
+        {
+
+            byte[] bytes = System.Text.Encoding.UTF8.GetBytes(Password + Salt);
+            SHA256Managed SHA256String = new SHA256Managed();
+            byte[] hash = SHA256String.ComputeHash(bytes);
+
+            return Convert.ToBase64String(hash);
+
+        }
+        public Customer FindCustomerByEmail(string email)
+        {
+            Customer c = new Customer();
+            List<Customers> GetAllCustomers = bmx.Customers.ToList();
+            for (int i = 0; i < GetAllCustomers.Count; i++)
+            {
+                if (GetAllCustomers[i].Mail == email)
+                {
+                    c.customerId = GetAllCustomers[i].CustomerId;
+                    c.email = email;
+                    c.firstname = GetAllCustomers[i].Firstname;
+                    c.lastname = GetAllCustomers[i].Lastname;
+                    c.address = GetAllCustomers[i].Address;
+                    c.isadmin = GetAllCustomers[i].IsAdmin;
+                    c.zipcode = GetAllCustomers[i].Zipcode;
+                    c.postalarea = GetAllCustomers[i].Postalareas.ToString();
+                    c.password = GetAllCustomers[i].Password;
+                    c.salt = GetAllCustomers[i].Salt;
+                    return c;
+                }
+            }
+            return null;
+        }
+        public Customers FindCustomersByEmail(string email)
+        {
+            List<Customers> GetAllCustomers = bmx.Customers.ToList();
+            for(int i = 0; i<GetAllCustomers.Count; i++)
+            {
+                if(GetAllCustomers[i].Mail == email)
+                {
+                    return GetAllCustomers[i];
+                }
+            }
+            return null;
+        }
+
+        public int CurrentCustomerId()
+        {
+            Customer c = (Customer)context.Session["CurrentUSer"];
+            return c.customerId;
+            //Endret her fra Customers til Customer for å teste
+        }
+    }
+}
---
+++ nettbutikk/DAL/CustomerRepoStub.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using nettButikkpls.Models;
+using System.Security.Cryptography;
+using System.Diagnostics;
+using System.Web.Mvc;
+
+namespace nettButikkpls.DAL
+{
+    public class CustomerRepoStub
+    {
+        public List<Customer> allCustomers()
+        {
+            using (var db = new NettbutikkContext())
+            {
+                List<Customer> allCustomers = new List<Customer>();
+                Customer c = new Customer()
+                {
+                    email = "daniel@thoresen.no",
+                    password = "Sommeren2015",
+                    firstname = "Daniel",
+                    lastname = "Thoresen",
+                    address = "Hesselbergs gate 7A",
+                    isadmin = false,
+                    salt = "hejhejhallo",
+                    zipcode = "0555",
+                    postalarea = "Oslo",
+                };
+                allCustomers.Add(c);
+                allCustomers.Add(c);
+                allCustomers.Add(c);
+                return allCustomers;
+            }
+        }
+        public bool saveCustomer (Customer inCustomer)
+        {
+                if (inCustomer.firstname == null)
+                    return false;
+                else
+                    return true;    
+        }
+
+        public bool EditCustomer(FormCollection inList)
+        {
+            if ((String.IsNullOrEmpty(inList["Firstname"])))
+                return false;
+            else
+                return true;
+        }
+        public bool Login()
+        {
+            Random rand = new Random();
+            int rnd = rand.Next(1, 5);
+            if (rnd < 3)
+                return false;
+            else if (rnd > 3)
+                return true;
+            else
+                return true;
+        }
+        public bool ValidateUser(FormCollection inList)
+        {
+            if ((String.IsNullOrEmpty(inList["Firstname"])))
+                return true;
+            else
+                return false;
+        }
+        public String GenerateSalt(int size)
+        {
+            var RandomNumberGenerator = new System.Security.Cryptography.RNGCryptoServiceProvider();
+            var buffer = new byte[size];
+            RandomNumberGenerator.GetBytes(buffer);
+            return Convert.ToBase64String(buffer);
+        }
+        public String HashPassword(String Password, string Salt)
+        {
+
+            byte[] bytes = System.Text.Encoding.UTF8.GetBytes(Password + Salt);
+            SHA256Managed SHA256String = new SHA256Managed();
+            byte[] hash = SHA256String.ComputeHash(bytes);
+
+            return Convert.ToBase64String(hash);
+
+        }
+        public Customer FindCustomerByEmail(string email)
+        {
+            /*List<Customer> clist = allCustomers();
+            Customer c = new Customer();
+            email = "daniel@thoresen.no";
+            for(int i = 0; i < clist.Count; i++)
+            {
+                if (clist[i].email == email)
+                {
+                    c.customerId = clist[i].customerId;
+                    c.email = email;
+                    c.firstname = clist[i].firstname;
+                    c.lastname = clist[i].lastname;
+                    c.address = clist[i].address;
+                    c.isadmin = clist[i].isadmin;
+                    c.zipcode = clist[i].zipcode;
+                    c.postalarea = clist[i].postalarea.ToString();
+                    c.password = clist[i].password;
+                    c.salt = clist[i].salt;
+                    return c;
+                }
+            }
+            return null;*/
+            Customer c = new Customer();
+            c.email = email;
+            return c;
+        }
+        public Customers FindCustomersByEmail(string email)
+        {
+            Customers c = new Customers();
+            c.Mail = email;
+            return c;
+        }
+
+        public int CurrentCustomerId()
+        {
+            int customerId = 1337;
+            return customerId;
+        }
+    }
+}
