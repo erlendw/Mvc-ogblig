237fb74a4a28290b3261ce2c7dfed3b19c0c5cf3
erlendw <erlendwestbye@gmail.com>
2015-11-08 21:02:57 +0100

    .. .. ..

--- nettbutikk/DAL/ProductRepo.cs
+++ nettbutikk/DAL/ProductRepo.cs
@@ -48,7 +48,7 @@ namespace nettButikkpls.DAL
                     newProductRow.Productname = inProduct.productname;
                     newProductRow.Description = inProduct.description;
                     newProductRow.Price = inProduct.price;
-                    newProductRow.Category = inProduct.category;               
+                    newProductRow.Category = inProduct.category;      
                     db.Products.Add(newProductRow);
                     db.SaveChanges();
 
@@ -101,7 +101,7 @@ namespace nettButikkpls.DAL
             }
             catch (Exception e)
             {
-                Debug.Write(e.ToString());
+                SaveToErrorLog(e.ToString());
                 return false;
             }
             
